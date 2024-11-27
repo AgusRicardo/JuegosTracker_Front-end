3a54c398b8fe188664080f8c36c49935b191e69e
Agustin Ricardo <agustinricardo14@gmail.com>
2024-11-27 20:28:42 -0300

    TK: Se consume la api desde el front, nuevo loading

--- src/pages/Steam/Steam.tsx
+++ src/pages/Steam/Steam.tsx
@@ -1,8 +1,66 @@
+import { useEffect, useState } from "react";
+import { getGames } from "../../services/public.services";
+import { Game } from "../../interfaces/game.types";
+import { Loading } from "../../components/Loading/Loading";
+import { AvailableStoresEnum } from "../../Helpers/Enums/AvailableStoresEnum";
 
 const Steam = () => {
-  return (
-    <div>Steam</div>
-  )
-}
+  const [games, setGames] = useState<Game[]>([]); 
+  const [isLoading, setIsLoading] = useState<boolean>(true); 
 
-export default Steam
+  useEffect(() => {
+    const fetchGames = async () => {
+      setIsLoading(true); 
+      try {
+        const gamesData = await getGames(AvailableStoresEnum.STEAM);
+        setGames(gamesData);
+      } catch (error) {
+        console.error("Error al cargar los juegos:", error);
+      } finally {
+        setIsLoading(false); 
+      }
+    };
+  
+    fetchGames();
+  }, []);
+
+  return (
+    <>
+      <div className="">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <h2 className="sr-only">Products</h2>
+  
+          {isLoading ? (
+            <div className="flex justify-center items-center">
+              <Loading />
+            </div>
+          ) : games.length === 0 ? ( 
+            <div className="text-center">
+              <p className="text-lg font-medium text-gray-700">No se encontraron juegos.</p>
+            </div>
+          ) : (
+            <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+              {games.map((product) => (
+                <div key={product.game_id} className="group">
+                  <img
+                    alt={product.game}
+                    src={product.img}
+                    className="aspect-square w-full rounded-lg bg-gray-200 object-cover xl:aspect-[7/8]"
+                  />
+                  <h3 className="mt-4 text-sm text-gray-700">{product.game}</h3>
+                  <div className="flex justify-end">
+                    <button type="button" className="flex items-center justify-center bg-gray-200 text-gray-600 hover:bg-gray-300 focus:outline-none">
+                      Borrar
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );  
+};
+
+export default Steam;
--- src/services/public.services.tsx
+++
@@ -1,8 +0,0 @@
-
-export const getGames = () => {
-  
-};
-
-export const getPlatforms = () => {
-  
-};
