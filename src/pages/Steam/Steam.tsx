665aaf972dbb14d66045d186933c8b2c4c230416
Gisela <giselladieguez2@gmail.com>
2024-12-03 15:38:14 -0300

    Se acomodan estilos de boton de borrado

--- src/pages/Steam/Steam.tsx
+++ src/pages/Steam/Steam.tsx
@@ -82,7 +82,7 @@ const Steam = () => {
                     <button
                       type="button"
                       onClick={() => openModal(product.game_id)}
-                      className="flex items-center justify-center bg-gray-200 text-gray-600 hover:bg-gray-300 focus:outline-none"
+                      className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                     >
                       Borrar
                     </button>
